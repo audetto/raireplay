0ca4076ce1deb2a363df3f19892e05d8b4e9c133
Andrea Odetti <mariofutire@googlemail.com>
2012-09-20 20:11:08 +0000

    First version of raireplay to download programs from http://www.rai.tv/dl/replaytv/replaytv.html
    Andrea
    
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@289 4d501edd-990c-4f8e-b6a2-3c50872f52da

---
+++ raireplay.py
@@ -0,0 +1,85 @@
+#! /usr/bin/python
+
+import json
+import sys
+import os
+
+from datetime import date
+from datetime import timedelta
+from urlgrabber.grabber import URLGrabber
+
+root_folder = os.path.expanduser("~/.raireplay")
+channels = ["RaiUno", "RaiDue", "RaiTre", "RaiCinque"]
+base_url = "http://www.rai.it/dl/portale/html/palinsesti/replaytv/static"
+
+
+class Program:
+    def __init__(self, date, time, pid, minutes, name, h264, tablet):
+        self.date = date
+        self.time = time
+        self.pid = pid
+        self.minutes = minutes
+        self.name = name.encode('utf-8')
+        self.h264 = h264
+        self.tablet = tablet
+
+
+def process(filename, db):
+    f = open(filename, "r")
+    o = json.load(f)
+
+    for k1, v1 in o.iteritems():
+        if k1 == "now":
+            continue
+        if k1 == "defaultBannerVars":
+            continue
+
+        for k2, v2 in v1.iteritems():
+            for key, value in v2.iteritems():
+                name = value["t"]
+                secs = value["l"]
+                minutes = 0
+                if secs != "":
+                    minutes = int(secs) / 60 
+
+                h264 = value["h264"]
+                tablet = value["urlTablet"]
+                pid = value["i"]
+
+                if h264 != "" or tablet != "":
+                    p = Program(k2, key, pid, minutes, name, h264, tablet)
+                    db[pid] = p
+
+
+def download(db):
+    g = URLGrabber()
+    today = date.today()
+
+    for x in range(1, 8):
+        day = today - timedelta(days = x)
+        str_date = day.strftime("_%Y_%m_%d")
+
+        for channel in channels:
+            filename = channel + str_date + ".html"
+            url = base_url + "/" + filename
+            local_name = os.path.join(root_folder, filename) 
+
+            if not os.path.exists(local_name):
+                print "Getting filename..."
+                g.urlgrab(url, filename = local_name)
+
+            process(local_name, db)
+
+
+def display(db):
+    for p in db.itervalues():
+        print p.pid, p.name
+
+
+def main():
+    db = {}
+    download(db)
+    display(db)
+
+
+main()
