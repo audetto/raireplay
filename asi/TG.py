e7b61f405a7ea7534cd466475f22ff19e332b1b3
Andrea Odetti <mariofutire@googlemail.com>
2013-01-15 21:18:19 +0000

    Improve somhow the names of --tg. Add follow support.
    Andrea
    
    git-svn-id: https://andsoft.svn.sourceforge.net/svnroot/andsoft/trunk@352 4d501edd-990c-4f8e-b6a2-3c50872f52da

--- asi/TG.py
+++ asi/TG.py
@@ -6,69 +6,77 @@ import json
 
 import urlgrabber.progress
 
-from asi import Meter
+from asi import Item
 from asi import Utils
 from asi import Config
 from asi import Base
 
 infoUrl = "http://www.rai.tv/dl/RaiTV/iphone/assets/tg_json.js?NS=0-1-4c61b46e9a4ab09b25da2246ae52d31edb528475-5.1.1"
 
-def processSet(grabber, title, f, db):
+# try to guess a date from a description like
+# "TG2 ore 23:30 del 14/01/2013"
+def isThereADate(oldDate, description):
+    try:
+        delGiorno = description[-14:]
+        if delGiorno.find("del ") != 0:
+            return oldDate
+        possibleDate = delGiorno[4:].replace("-", "/")
+        newDate = datetime.datetime.strptime( possibleDate, "%d/%m/%Y")
+        strDate = newDate.strftime("%d/%m/%Y")
+        return strDate
+    except ValueError:
+        return oldDate
+
+def processSet(grabber, title, time, f, db):
     o = json.load(f)
 
     prog = o.get("integrale")
     if prog != None:
-        contentItem   = prog["weblink"]
+        link          = prog["weblink"]
         h264          = prog["h264"]
         m3u8          = prog["m3u8"]
-        date          = prog["date"]
+        if time == "LIS":
+            # strange time for some TG3
+            time = "00:00"
+
         description   = prog["name"]
+        date          = prog["date"]
+
+        # the filed "date" seems to be always TODAY
+        # so we might actually get a program from yesterday
+        date          = isThereADate(date, description)
+        datetime      = date + " " + time
 
         pid = str(len(db))
-        p = Program(grabber, "channel", date, pid, title, description, h264, m3u8)
+        p = Program(grabber, link, title, datetime, pid, title, description, h264, m3u8)
         db[p.pid] = p
     else:
         # get the list
         lst = o["list"]
 
         for prg in lst:
-            tp            = prg["type"]
+            tp = prg["type"]
             if tp != "empty":
-                contentItem   = prg["weblink"]
+                link          = prg["weblink"]
                 h264          = prg["h264"]
                 m3u8          = prg["m3u8"]
-                date          = prg["date"]
-                title         = prg["name"]
+                dt            = prg["date"] + " 00:00"
+                aTitle        = title + "-" + prg["name"]
                 description   = prg["desc"]
 
                 pid = str(len(db))
-                p = Program(grabber, "channel", date, pid, title, description, h264, m3u8)
+                p = Program(grabber, link, title, dt, pid, aTitle, description, h264, m3u8)
                 db[p.pid] = p
 
 
-def processSetIntegrale(grabber, title, f, db):
-    o = json.load(f)
-
-    prog          = o["integrale"]
-    contentItem   = prog["weblink"]
-    h264          = prog["h264"]
-    m3u8          = prog["m3u8"]
-    date          = prog["date"]
-    description   = prog["name"]
-
-    pid = str(len(db))
-    p = Program(grabber, "channel", date, pid, title, description, h264, m3u8)
-    db[p.pid] = p
-
-
-def processItem(grabber, progress_obj, downType, title, url, db):
+def processItem(grabber, progress_obj, downType, title, time, url, db):
     folder = Config.tgFolder
 
     name = Utils.httpFilename(url)
     localName = os.path.join(folder, name)
 
     f = Utils.download(grabber, progress_obj, url, localName, downType, "utf-8", True)
-    processSet(grabber, title, f, db)
+    processSet(grabber, title, time, f, db)
 
 
 def processGroup(grabber, progress_obj, downType, prog, db):
@@ -79,9 +87,9 @@ def processGroup(grabber, progress_obj, downType, prog, db):
     if edizioni != None:
         for time, url in edizioni.iteritems():
             title = name + " " + time
-            processItem(grabber, progress_obj, downType, title, url, db)
+            processItem(grabber, progress_obj, downType, title, time, url, db)
     elif dettaglio.find("ContentSet") >= 0:
-        processItem(grabber, progress_obj, downType, None, dettaglio, db)
+        processItem(grabber, progress_obj, downType, name, None, dettaglio, db)
 
 
 def process(grabber, progress_obj, downType, f, db):
@@ -105,15 +113,16 @@ def download(db, grabber, downType):
 
 
 class Program(Base.Base):
-    def __init__(self, grabber, channel, date, pid, title, desc, h264, m3u8):
+    def __init__(self, grabber, link, channel, date, pid, title, desc, h264, m3u8):
         super(Program, self).__init__()
 
+        self.link = link
         self.pid = pid
         self.title = title
         self.description = desc
         self.channel = channel
         strtime = date.replace("-", "/")
-        self.datetime = datetime.datetime.strptime(strtime, "%d/%m/%Y")
+        self.datetime = datetime.datetime.strptime(strtime, "%d/%m/%Y %H:%M")
         if h264:
             self.h264 = h264
         if m3u8:
@@ -122,7 +131,7 @@ class Program(Base.Base):
         self.grabber = grabber
 
         name = Utils.makeFilename(self.title)
-        self.filename = self.pid + "-" + name
+        self.filename = name + "-" + self.datetime.strftime("%Y-%m-%d")
 
 
     def display(self):
@@ -135,9 +144,15 @@ class Program(Base.Base):
         print("Description:", self.description)
         print("Date:", self.datetime.strftime("%Y-%m-%d %H:%M"))
         print("Filename:", self.filename)
+        print("Link:", self.link)
         print()
         print("h264:", self.h264)
         print("m3u8:", self.ts)
 
         m3 = self.getTabletPlaylist()
         Utils.displayM3U8(self.m3)
+
+
+    def follow(self, db, downType):
+        p = Item.Demand(self.grabber, self.link, downType, self.pid)
+        db[str(self.pid)] = p
