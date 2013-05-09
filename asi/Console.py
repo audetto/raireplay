9e96f92db3e2b833c9fcc353c1558bd4d5aee8ce
Andrea Odetti <mariofutire@googlemail.com>
2013-05-09 20:10:41 +0100

    Python 3.3 has a new method to get the terminal size. Use it if available, otherwise we revert to the Linux only old implementation.
    Andrea

--- asi/Console.py
+++ asi/Console.py
@@ -1,10 +1,13 @@
-import fcntl
-import struct
-import termios
+import sys
 
 # Code from http://mail.python.org/pipermail/python-list/2000-May/033365.html
-def terminal_width(fd=1):
+def terminal_width_linux(fd=1):
     """ Get the real terminal width """
+
+    import fcntl
+    import struct
+    import termios
+
     try:
         buf = 'abcdefgh'
         buf = fcntl.ioctl(fd, termios.TIOCGWINSZ, buf)
@@ -15,3 +18,17 @@ def terminal_width(fd=1):
         return ret
     except: # IOError
         return 80
+
+def terminal_width_native():
+    import shutil
+
+    size = shutil.get_terminal_size((80, 24))
+
+    return size.columns
+
+def terminal_width():
+    ver = sys.version_info
+    if ver >= (3, 3):
+        return terminal_width_native()
+    else:
+        return terminal_width_linux()
