2eff2e4d6a3396312b485656752af34911ece557
Andrea Odetti <mariofutire@googlemail.com>
2013-06-08 20:02:25 +0100

    We handle more Mediaset programs.
    Andrea

--- asi/Mediaset.py
+++ asi/Mediaset.py
@@ -10,6 +10,11 @@ from asi import Config
 
 configUrl = "http://app.mediaset.it/app/videomediaset/iPhone/2.0.2/videomediaset_iphone_config.plist"
 
+FULL_VIDEO = 0
+PROGRAM_LIST = 1
+PROGRAM = 2
+PROGRAM_VIDEO = 3
+
 def parseConfig(root):
     dic = root.find("dict")
 
@@ -30,10 +35,10 @@ def parseConfig(root):
     return result
 
 
-def processVideos(grabber, f, conf, folder, progress, downType, db):
+def processFullVideo(grabber, f, tag, conf, folder, progress, downType, db):
     o = json.load(f)
 
-    videos = o["episodi_interi"]["video"]
+    videos = o[tag]["video"]
 
     for v in videos:
         title = v["brand"]["value"] + " " + v["title"]
@@ -43,25 +48,43 @@ def processVideos(grabber, f, conf, folder, progress, downType, db):
         length = v["duration"]
         num = v["id"]
 
-        pid = Utils.getNewPID(db, num)
-        p = Program(grabber, conf, date, length, pid, title, desc, num, channel)
-        Utils.addToDB(db, p)
+        category = v["subbrand"]["name"]
+
+        if category == "full":
+            pid = Utils.getNewPID(db, num)
+            p = Program(grabber, conf, date, length, pid, title, desc, num, channel)
+            Utils.addToDB(db, p)
 
 
-def processGroup(grabber, f, conf, folder, progress, downType, db):
+def processProgramList(grabber, f, conf, folder, progress, downType, db):
     o = json.load(f)
 
     for a in o["programmi"]["programma"]:
-        print(a["brand"]["value"], a["urlxml"])
+        url = a["urlxml"]
+        downloadItems(grabber, url, PROGRAM, conf, folder, progress, downType, db)
 
 
-def downloadGroup(grabber, url, conf, folder, progress, downType, db):
+def processProgram(grabber, f, conf, folder, progress, downType, db):
+    o = json.load(f)
+
+    url = o["brandinfo"]["url_xmlvideo"]
+    downloadItems(grabber, url, PROGRAM_VIDEO, conf, folder, progress, downType, db)
+
+
+def downloadItems(grabber, url, which, conf, folder, progress, downType, db):
     name = Utils.httpFilename(url)
     localName = os.path.join(folder, name)
 
     f = Utils.download(grabber, progress, url, localName, downType, "utf-8", True)
 
-    processVideos(grabber, f, conf, folder, progress, downType, db)
+    if which == FULL_VIDEO:
+        processFullVideo(grabber, f, "episodi_interi", conf, folder, progress, downType, db)
+    elif which == PROGRAM_LIST:
+        processProgramList(grabber, f, conf, folder, progress, downType, db)
+    elif which == PROGRAM:
+        processProgram(grabber, f, conf, folder, progress, downType, db)
+    elif which == PROGRAM_VIDEO:
+        processFullVideo(grabber, f, "brand", conf, folder, progress, downType, db)
 
 
 def download(db, grabber, downType):
@@ -76,11 +99,11 @@ def download(db, grabber, downType):
     root = ElementTree.fromstring(s)
     conf = parseConfig(root)
 
-#    programs = conf["ProgramListRequestUrl"]
-#    downloadGroup(grabber, programs, folder, progress, downType, db)
+    url = conf["ProgramListRequestUrl"]
+    downloadItems(grabber, url, PROGRAM_LIST, conf, folder, progress, downType, db)
 
-    episodes = conf["FullVideoRequestUrl"].replace("http://ww.", "http://www.")
-    downloadGroup(grabber, episodes, conf, folder, progress, downType, db)
+    url = conf["FullVideoRequestUrl"].replace("http://ww.", "http://www.")
+    downloadItems(grabber, url, FULL_VIDEO, conf, folder, progress, downType, db)
 
 
 def getMediasetLink(conf, num):
