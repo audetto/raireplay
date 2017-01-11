c735f34272132c9878ddb93a50092188f9470bec
Andrea Odetti <mariofutire@gmail.com>
2017-01-11 21:13:58 +0000

    Only list 1 copy of each program and only if there is a video. Handle some strange error RAI outputs in the json files:
    [an error occurred while processing this directive]
    
    mah...
    
    Andrea

--- asi/RaiPlay.py
+++ asi/RaiPlay.py
@@ -9,36 +9,46 @@ from asi import Config
 from asi import Base
 from asi import RAIUrls
 
-channels = ["Rai1", "Rai3", "Rai4", "Rai5", "RaiGulp", "RaiPremium", "RaiYoyo", "RaiSport1", "RaiSport2", "RaiMovie", "RaiStoria", "RaiScuola"]
+channels = ["Rai1", "Rai2", "Rai3", "Rai4", "Rai5", "RaiGulp", "RaiPremium", "RaiYoyo", "RaiSport1", "RaiSport2", "RaiMovie", "RaiStoria", "RaiScuola"]
 
-def parseItem(grabber, downType, channel, value, db):
-    name = value["name"]
-    desc = value["description"]
-    secs = value["duration"]
+def parseItem(grabber, downType, channel, value, db, dup):
+    if value and "hasVideo" in value and value["hasVideo"]:
 
-    date = value["datePublished"]
-    time = value["timePublished"]
+        id = value["ID"]
 
-    length = secs
+        if not id in dup:
 
-    pathID = value["pathID"]
+            dup.add(id)
 
-    pid = Utils.getNewPID(db, None)
-    p = Program(grabber, downType, channel, date, time, pid, length, name, desc, pathID)
-    Utils.addToDB(db, p)
+            name = value["name"]
+            desc = value["description"]
+            secs = value["duration"]
+
+            date = value["datePublished"]
+            time = value["timePublished"]
+
+            length = secs
+
+            pathID = value["pathID"]
+
+            pid = Utils.getNewPID(db, None)
+            p = Program(grabber, downType, channel, date, time, pid, length, name, desc, pathID)
+            Utils.addToDB(db, p)
 
 
-def process(grabber, downType, f, db):
-    o = json.load(f)
+def process(grabber, downType, f, db, dup):
+    s = f.read()
+    s = s.replace('[an error occurred while processing this directive]', '')
+    o = json.loads(s)
 
     for k1, v1 in o.items():
         channel = k1
         for v2 in v1:
-            date = v2['palinsesto'][0]['giorno']
-            programmi = v2['palinsesto'][0]['programmi']
-            for p in programmi:
-                if p:
-                    parseItem(grabber, downType, channel, p, db)
+            palinsesto = v2['palinsesto']
+            if palinsesto:
+                programmi = palinsesto[0]['programmi']
+                for p in programmi:
+                    parseItem(grabber, downType, channel, p, db, dup)
 
 
 def download(db, grabber, downType):
@@ -46,14 +56,16 @@ def download(db, grabber, downType):
 
     folder = Config.raiplayFolder
 
+    dup = set()
+
     for channel in channels:
         filename = "canale=" + channel
         url = RAIUrls.raiplay + filename
         localName = os.path.join(folder, filename + ".json")
-        f = Utils.download(grabber, progress, url, localName, downType, "utf-8")
+        f = Utils.download(grabber, progress, url, localName, downType, "utf-8", True)
 
         if f:
-            process(grabber, downType, f, db)
+            process(grabber, downType, f, db, dup)
 
 
 class Program(Base.Base):
@@ -75,6 +87,7 @@ class Program(Base.Base):
 
         name = Utils.makeFilename(self.title)
         self.filename = self.pid + "-" + name
+
 
     def getTS(self):
         if self.ts:
