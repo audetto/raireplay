a244fbf96a0471c1637cdd29e3609dc924ced5b7
Andrea Odetti <mariofutire@gmail.com>
2020-01-03 09:48:52 +0000

    Update single programma to the new RaiPlay format.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- asi/services/programma.py
+++ asi/services/programma.py
@@ -2,7 +2,7 @@ import os
 import urllib.parse
 import json
 
-from asi import utils
+from asi import utils, raiurls
 from asi import config
 from asi.services import raiplay
 
@@ -10,7 +10,7 @@ from asi.services import raiplay
 def process_set(grabber, down_type, f, db):
     o = json.load(f)
 
-    channel = o["Name"]
+    channel = o["name"]
     items = o["items"]
 
     for value in items:
@@ -18,12 +18,12 @@ def process_set(grabber, down_type, f, db):
         desc = ""
         secs = value["duration"]
 
-        date = value["datePublished"].strip()
-        time = value["timePublished"].strip()
+        date = None
+        time = None
 
         length = secs
 
-        path_id = value["pathID"]
+        path_id = value["path_id"]
 
         pid = utils.get_new_pid(db, None)
         p = raiplay.Program(grabber, down_type, channel, date, time, pid, length, name, desc, path_id)
@@ -33,13 +33,13 @@ def process_set(grabber, down_type, f, db):
 def process(grabber, progress, folder, down_type, f, db):
     o = json.load(f)
 
-    blocks = o["Blocks"]
+    blocks = o["blocks"]
 
     for v1 in blocks:
-        if "Sets" in v1:
-            for v2 in v1["Sets"]:
-                path = v2["url"]
-                url = "http://www.raiplay.it/" + path
+        if "sets" in v1:
+            for v2 in v1["sets"]:
+                path = v2["path_id"]
+                url = raiurls.raiplay_base + "/" + path
 
                 filename = path.replace("/", "_")
                 local_name = os.path.join(folder, filename + ".json")
@@ -55,7 +55,7 @@ def download(db, grabber, programma, down_type):
 
     o = urllib.parse.urlparse(programma)
     filename = o.path.replace("/", "_")
-    url = programma + "/?json"
+    url = programma + ".json"
     local_name = os.path.join(folder, filename + ".json")
 
     f = utils.download(grabber, progress, url, local_name, down_type, "utf-8", True)
