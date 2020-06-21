cd8234245476fb305286d982e8e7c34a1f065660
Andrea Odetti <mariofutire@gmail.com>
2020-06-21 11:52:14 +0100

    Add some comments about RAI Radio Urls.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- raireplay/common/raiurls.py
+++ raireplay/common/raiurls.py
@@ -8,3 +8,10 @@ raiplay = "https://www.raiplay.it/palinsesto/app/${channel}/${date}.json"
 
 invalidMP4 = {"http://creativemedia3.rai.it/video_no_available.mp4",
               '<Mediapolis><url type="content">http://download.rai.it/video_no_available.mp4</url><url type="bumper"></url><url type="bumperend"></url><ct>mp4</ct><bitrate>0</bitrate><smooth>N</smooth><is_live>N</is_live><description>Rai</description><geoprotection>N</geoprotection><category>11111</category><plVod></plVod><plLive></plLive><logo></logo><poster>http://www.rai.tv/dl/RaiTV/2012/images/ogImage.jpg</poster><duration></duration><krpano></krpano></Mediapolis>'}
+
+
+# raiplayradio
+# root:
+# http://www.rai.it/dl/rairadio/mobile/config/RaiRadioConfig.json
+# dirette:
+# https://www.raiplayradio.it/dirette/ContentSet-8e2a3414-bbfd-4a72-852b-7d827dc00e7e.html?json
